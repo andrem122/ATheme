8df6e6a39bc61073a0ab60947533795f06f71fa5
andrem122 <andre.mashraghi@gmail.com>
2017-07-10 19:36:23 -0400

    c

--- assets/js/a-theme.js
+++ assets/js/a-theme.js
@@ -1,10 +1,22 @@
 (function($) {
   $(function() {
+    var $window = $(window);
+    var $nav = $('header.top');
     var $headerTop = $('header.top');
     var $mobileMenuButton = $headerTop.find('.mobile-menu-button');
     var $mobileMenu = $headerTop.find('.mobile-menu');
+    //mobile menu
     $mobileMenuButton.click(function(){
       $mobileMenu.slideToggle(200);
+    });
+    //header animation
+    $window.scroll(function() {
+      if($(this).scrollTop() >= 500) {
+        $nav.addClass('sticky header-top-in-a')
+        .removeClass('header-top-out-a');
+      } else {
+        $nav.removeClass('sticky header-top-in-a');
+      }
     });
   });
 })( jQuery );
