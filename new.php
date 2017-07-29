7258a33157bb6c4f1b7a3474cdf770d2200c559c
andrem122 <andre.mashraghi@gmail.com>
2017-07-29 11:29:12 -0400

    finished

--- new.php
+++ new.php
@@ -1,12 +1,25 @@
 <?php
-   /*
-   Plugin Name: Atheme Shortcodes
-   Description: A plugin for Atheme shortcodes
-   Version: 1.0
-   Author: Andre Mashraghi
-   Author URI: http://andremashraghi.com/
-   License: GPL2
-   */
+  /*
+  Plugin Name: Atheme Shortcodes
+  Description: A plugin for Atheme shortcodes
+  Version: 1.0
+  Author: Andre Mashraghi
+  Author URI: http://andremashraghi.com/
+  License: GPL2
+
+  Atheme Shortcodes is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  any later version.
+
+  Atheme Shortcodes is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Atheme Shortcodes. If not, see https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html.
+  */
 
 function atheme_shortcodes_init() {
   add_shortcode('recent_posts', 'atheme_recent_posts');
@@ -25,8 +38,14 @@ function atheme_shortcodes_init() {
 
 add_action('init', 'atheme_shortcodes_init');
 
+function atheme_shortcodes_delete() {
+  remove_all_shortcodes();
+}
+
+//call the atheme_shortcodes_delete function on deletion of the plugin
+register_uninstall_hook(__FILE__, 'atheme_shortcodes_delete');
+
 //displays recent posts
-//shortcode [recent-posts number_posts="3"]
 function atheme_recent_posts($atts){
   // normalize attribute keys, lowercase
   $atts = array_change_key_case( (array)$atts, CASE_LOWER );
